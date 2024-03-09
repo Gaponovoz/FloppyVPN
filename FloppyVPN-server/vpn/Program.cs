ab894debe6cfe4db0e02744bf9f090ca5a39ce1d
Gaponovoz <gaponovoz@i.ua>
2024-03-10 02:16:12 +0300

    Making movies

--- FloppyVPN-server/vpn/Program.cs
+++ FloppyVPN-server/vpn/Program.cs
@@ -4,11 +4,19 @@ namespace FloppyVPN
     {
         static void Main()
         {
+			//single instance:
+			Mutex mutex = new(true, "floppyvpn_vpn_L2Neli82o5I8P1hsqVtOHoq67htydhythtdeyj8AuWYE", out bool result);
+			if (!result)
+			{
+				Console.WriteLine("The same server is already running so it won't start again.");
+				Environment.Exit(0);
+			}
+			GC.KeepAlive(mutex);
 
 
 
 
-            new Thread(() => Listener.Start()).Start();
+			new Thread(() => Listener.Start()).Start();
         }
     }
 }
---
+++ FloppyVPN-server/website/Views/_Functions.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace FloppyVPN
+{
+	public static class _Functions
+	{
+		public static string GetCurrentLanguage(RazorPage page)
+		{
+			string? languageFromUrl = page.Context.Request.Query["lang"].ToString();
+			string detectedLang = "en";
+
+			if (!string.IsNullOrEmpty(languageFromUrl))
+			{
+				detectedLang = languageFromUrl;
+			}
+
+			string? languageFromCookie = ReadCookie(page, "language");
+
+			if (!string.IsNullOrEmpty(languageFromCookie))
+			{
+				detectedLang = languageFromCookie;
+			}
+
+			decision:
+
+			if (Loc.table != null && Loc.table.Columns.Contains(detectedLang))
+			{
+				if (languageFromCookie != languageFromUrl && languageFromCookie == null && languageFromUrl != null)
+					WriteCookie(page, "language", languageFromUrl);
+
+				return detectedLang;
+			}
+			else
+			{
+				return "en";
+			}
+		}
+
+		public static void WriteCookie(RazorPage page, string key, string value)
+		{
+			page.Context.Response.Cookies.Append(key, value, new CookieOptions() { Expires = DateTimeOffset.MaxValue });
+		}
+
+		public static string ReadCookie(RazorPage page, string key)
+		{
+			return page.Context.Request.Cookies[key];
+		}
+	}
+}
